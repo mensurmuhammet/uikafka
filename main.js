934f54fea82acd759ae3aeeb3c17046cf088d1bf
mensurmuhammet <52221501+mensurmuhammet@users.noreply.github.com>
2020-08-13 10:40:07 +0300

    Interface Design

---
+++ main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Tarayıcı penceresini oluştur.
+  let win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    frame:false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // ve uygulamanın index.html dosyasını yükle.
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
